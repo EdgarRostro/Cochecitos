8fd31907eb9952e0513189f14f4e9d8bacc9341b
Valeria Pineda <57368055+vpinedagon2000@users.noreply.github.com>
2021-11-22 13:40:34 -0600

    finish python agents
    Co-authored-by: Edgar Rostro <EdgarRostro@users.noreply.github.com>
    Co-authored-by: Eduardo Villalpando Mello <edvilme@users.noreply.github.com>

--- cerebritos/integradora/boxes.py
+++ cerebritos/integradora/boxes.py
@@ -1,47 +1,55 @@
 from mesa import Agent
+import random
 
 class Cell(Agent):
     
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        # Conditions = Unvisited, visited, pile
-        self.state = "Unvisited"
+        # Conditions = Available, Obstacle
+        self.condition = "Available"
+
+    def step(self):
+        # Obtener vecinos (lista de coordenadas)
+        neighbors = self.model.grid.get_neighborhood(self.pos, moore=False, include_center=False)
+        # Contar vecinos con obstaculos
+        count = 0
+        for coords in neighbors:
+            # Obtener agentes en los vecinos
+            cellAgents = self.model.grid[coords[0]][coords[1]]        
+            # Incrementar count cuando hay más de dos agentes (obstáculo)
+            hasObstacle = False
+            for agent in cellAgents:
+                if agent.condition not in ["Moving", "Available", "Placing"]:
+                    hasObstacle += True
+            if hasObstacle:
+                count+=1
+        # Si hay 3+ vecinos con obstáculos, hacer celda un obstáculo
+        if count >=3:
+            self.condition = "Obstacle"
+        else:
+            self.condition = "Available"
 
 
 class Box(Agent):
 
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        # Conditions = Unplaced, Moving, Piled
+        # Conditions = Unplaced, Piled, Moving
         self.condition = "Unplaced"
 
     def step(self):
         pass
 
-leftTurns = {
-    0: 1, 
-    1: 3, 
-    2: 0, 
-    3: 2
-}
-
-rightTurns = {
-    0: 2, 
-    2: 3, 
-    3: 1, 
-    1: 0
-}
 
 class Robot(Agent):
-
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        # Conditions = Searching, Placing, Returning
+        # Conditions = Searching, Placing
         self.condition = "Searching"
         self.box = None
         # Direction: N=0, W=1, E=2, S=3
         self.direction = 0
-        self.lastPos = (0, 0)
+        self.moves = 0
 
     # Determina si un vecino es obstáculo y lo regresa
     def isNeighborObstacle(self, cell):
@@ -50,11 +58,14 @@ class Robot(Agent):
             return (True, None)
         # Obtener agentes en cell
         cellAgents = self.model.grid[cell[0]][cell[1]]
+        # Obtener agente celda
+        if cellAgents[0].condition == "Obstacle":
+            return (True, cellAgents[0])
         # Si el tamaño es uno, la celda está vacía
-        if len(cellAgents) >= 5:
+        if len(cellAgents) >= 6:
             return (True, cellAgents[0])
         # Checar si alguna celda es obstáculo
-        ans = ()
+        ans = (False, cellAgents[0])
         for agent in cellAgents:
             # Siempre: Otro robot
             if isinstance(agent, Robot):
@@ -69,10 +80,10 @@ class Robot(Agent):
         deltaX = self.pos[0] - coords[0]
         deltaY = self.pos[1] - coords[1]
         # Obtener direccion (-1, o 1)
-        dirX = -1 if deltaX < 0 else 1
-        dirY = -1 if deltaY < 0 else 1
+        dirX = 1 if deltaX < 0 else -1
+        dirY = 1 if deltaY < 0 else -1
         # Almacenar nuevas coordenadas
-        newCoords = None
+        newCoords = self.pos
         if deltaX == 0:
             newCoords = (self.pos[0], self.pos[1] + dirY)
         elif deltaY == 0:
@@ -83,15 +94,20 @@ class Robot(Agent):
             newCoords = (self.pos[0], self.pos[1] + dirY)    
         else:
             newCoords = (self.pos[0] + dirX, self.pos[1])
-        # Si hay un obstáculo en esas coordenadas
-        isObstacle = self.isNeighborObstacle(newCoords)[0]
-        if isObstacle[0]:
-            
         
-
+        # Si hay un obstáculo en esas coordenadas ??
+        isObstacle = self.isNeighborObstacle(newCoords)[0]
+        while isObstacle:
+            # Obtener vecino random
+            neighbors = self.model.grid.get_neighborhood(self.pos, moore=False, include_center=False)
+            # Obtener elemento random de arreglo
+            newCoords = random.choice(neighbors)
+            # Actualizar condicion
+            isObstacle = self.isNeighborObstacle(newCoords)[0]
         return newCoords
 
     def move(self):
+        
         # Obtener vecinos [N, W, E, S]
         neighbors = self.getNeighbors()
         # Establecer direccion
@@ -100,16 +116,29 @@ class Robot(Agent):
         # Determinar movimiento cuando está buscando
         # Si está buscando, moverse en dirección aleatoria hasta encontrar una caja
         if self.condition == "Searching":
-            newDir = self.random.randint(0,3)
-            obs = self.isNeighborObstacle(neighbors[newDir])
-            if obs[0]:
+            newPos = ()
+            # Buscar alguna celda con caja e irse hacia allá
+            for neighbor in self.model.grid.neighbor_iter(self.pos, moore=False):
+                if neighbor.condition == "Unplaced":
+                    newPos = neighbor.pos
+                    break
+                else:
+                    newDir = self.random.randint(0,3)
+                    newPos = neighbors[newDir]
+
+            obs = self.isNeighborObstacle(newPos)
+            if obs[0] and obs[1] != None:
                 if obs[1].condition == "Unplaced":
-                    self.model.grid.move_agent(self, neighbors[newDir])
+                    self.model.grid.move_agent(self, newPos)
                     self.box = obs[1]
                     self.condition = "Placing"
-                    self.lastPos = self.pos
-            else:
-                self.model.grid.move_agent(self, neighbors[newDir])
+                    self.box.condition = "Moving"
+                elif isinstance(obs[1], Cell):
+                   self.model.grid.move_agent(self, newPos)
+                   self.moves += 1
+            elif not obs[0]:
+                self.model.grid.move_agent(self, newPos)
+                self.moves += 1
         
         # Viajando (con caja) a apilar la caja
         elif self.condition == "Placing":
@@ -120,71 +149,17 @@ class Robot(Agent):
                 self.model.grid.move_agent(self, moveHere)
                 # Mover caja
                 self.model.grid.move_agent(self.box, moveHere)
+                self.moves += 1
             else:
                 # Soltar caja y cambiar estado
+                self.box.condition = "Piled"
+                # Actualizar ubicacion de pila
+                self.model.check_pile()
                 # La caja se queda una posición a la izquierda de donde está el robot
                 pile = (self.model.pile[0]-1, self.model.pile[1])
                 self.model.grid.move_agent(self.box, pile)
-                self.condition = "Returning"
-        
-        # Regresando (sin caja)
-        else:
-            moveHere = self.moveToCoords(self.lastPos)
-            if self.pos != self.lastPos:
-                self.model.grid.move_agent(moveHere)
-            else:
                 self.condition = "Searching"
-
-
-        """
-        # FIjarnos en la celda de adelante (neighbor[self.direction])
-        # cAmbiar direccion cuando choca con pared (empezar a marcar visitadas)
-        if not self.model.grid.out_of_bounds(neighbors[0]):
-            if self.condition == "Starting":
-                self.condition = "Searching"
-                if not self.model.grid.out_of_bounds(neighbors[2]) or :
-                
-        
-        
-        # (intentar ir siempre a su izquierda, seguir derecho o derecha)            
-        # si celda a su izquierda está disponible -> newDir = leftTurns[self.direction]
-        # Si estas buscando y esa celda es una caja, agarrar y seguir con misma direccion
-
-
-        def tryTurnLeft():
-            # Intentar izquierda
-            leftCell = neighbors[leftTurns[self.direction]]
-            if not self.model.grid.out_of_bounds(leftCell):
-                leftCellAgents = self.model.grid[leftCell[0]][leftCell[1]]
-                for agent in leftCellAgents:
-                    if self.condition == "Searching" and agent.condition == "Unplaced":
-                        self.model.grid.move_agent(self, leftCell)
-                        return
-                    elif self.condition == "Searching" and agent.condition == "Unvisited":
-                        self.model.grid.move_agent(self, leftCell)
-                        return
-                    elif self.condition == "Placing" and len(leftCellAgents) == 1:
-                        self.model.grid.move_agent(self, leftCell)
-                        return
-                    
-                          
-            # Si izquierda no se puede, intentar seguir misma dirección
-            frontCell = neighbors[self.direction]
-            if not self.model.grid.out_of_bounds(frontCell):
-                frontCellAgents = self.model.grid[frontCell[0]][frontCell[1]]
-                for agent in frontCellAgents:
-                    return
-            # Si misma dirección no se puede intentar derecha
-            rightCell = neighbors[self.direction]
-            if not self. model.grid.out_of_bounds(rightCell):
-                rightCellAgents = self.model.grid[rightCell[0]][rightCell[1]]
-                for agent in rightCellAgents:
-                    return
-            # Si ninguna se puede (f) no moverse hasta el siguiente paso
-            return
-            """
-
-        
+                self.box = None
 
     def getNeighbors(self):
         # Obtener coordenadas de vecinos
@@ -197,7 +172,5 @@ class Robot(Agent):
 
     def step(self):
         # Mover en dirección 
-        # Si está buscando
-        if self.condition == "Searching":
-            # Obtener agentes que estén en la misma posición
-
+        self.move()
+    
--- cerebritos/integradora/model.py
+++ cerebritos/integradora/model.py
@@ -1,11 +1,83 @@
+from random import shuffle
 from mesa import Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation, SimultaneousActivation, StagedActivation
 from mesa.datacollection import DataCollector
-from numpy import show_config
 from boxes import Cell, Robot, Box
 
 class Room(Model):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, width, height, density, robots, timer):
         self.pile = (1,0)
+        self.pile_size = 0
+
+        # model_stages = ["stage_1", "stage_2"]
+        # self.schedule = StagedActivation(self, model_stages)
+        self.schedule = RandomActivation(self)
+
+        # Inicializar data colector
+        self.datacollector = DataCollector(
+            {
+                "Movimientos": lambda m: self.count_moves(m)
+            }
+        )
+
+        self.grid = MultiGrid(width, height, torus=False)
+        self.running = True
+        self.timer = 0
+        self.timeLimit = timer
+        self.steps = 0
+        self.id = 0
+
+        # Poner celdas en todo el grid
+        for(contents, x, y) in self.grid.coord_iter():
+            cell = Cell(self.id, self)
+            self.grid.place_agent(cell, (x, y))
+            self.schedule.add(cell)
+            self.id += 1
+        # Agregar cajas (según densidad)
+        for (contents, x, y) in self.grid.coord_iter():
+            if self.random.random() < density and not (x in [0, 1] and y == 0):
+                new_box = Box(self.id, self)
+                self.grid.place_agent(new_box, (x, y))
+                self.schedule.add(new_box)
+                self.id += 1
+        
+        # Poner cantidad necesaria de robots
+        for i in range(robots):
+            new_robot = Robot(self.id, self)
+            self.grid.place_agent(new_robot, (self.random.randint(0, width - 1), self.random.randint(0, height - 1)))
+            self.schedule.add(new_robot)
+            self.id += 1
+    
+    def check_pile(self):
+        self.pile_size += 1
+        if self.pile_size >= 5:
+            new_coords = int(self.pile[0]), self.pile[1]+1
+            if self.grid.out_of_bounds(new_coords):
+                new_coords = self.pile[0]+1, int(0)
+            self.pile = new_coords
+            self.pile_size = 0
+            
+    def step(self):
+        self.schedule.step()
+        count = 0
+        self.timer += 1
+        if self.timer >= self.timeLimit:
+            self.running = False
+        for agent in self.schedule.agents:
+            if agent.condition in ["Unplaced", "Moving"]:
+                count += 1
+        if count == 0:
+            self.running = False
+        
+        # Collect data on each step
+        self.datacollector.collect(self)
+
+    @staticmethod
+    def count_moves(model):
+        count = 0
+        for agent in model.schedule.agents:  
+            if isinstance(agent, Robot):
+                count += agent.moves
+
+        return count

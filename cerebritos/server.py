540112ac6bcfa8ae912c75386f5d522e17705d73
Eduardo Villalpando Mello <eduardo.villalpando.mello@gmail.com>
2021-11-30 16:46:43 -0600

    Fix bugs in server.py

--- cerebritos/server.py
+++ cerebritos/server.py
@@ -1,5 +1,5 @@
 from agent import *
-from model import RandomModel
+from model import City
 from mesa.visualization.modules import CanvasGrid, BarChartModule
 from mesa.visualization.ModularVisualization import ModularServer
 
@@ -52,7 +52,7 @@ model_params = {"N":1}
 
 grid = CanvasGrid(agent_portrayal, width, height, 500, 500)
 
-server = ModularServer(RandomModel, [grid], "Traffic Base", model_params)
+server = ModularServer(City, [grid], "Traffic Base", model_params)
                        
 server.port = 8521 # The default
 server.launch()
